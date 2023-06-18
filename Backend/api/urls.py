72daf9611668b95a553a571ba073eefe3faba520
Marcus Vinícius <76022387+marcusvcalves@users.noreply.github.com>
2023-06-18 12:28:25 -0300

    criação das urls endpoints para clientes

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path('mostrar-produto/<str:pk>', views.showProduct, name='mostrar-produto'),
     path('criar-produto/', views.createProduct, name='mostrar-produto'),
     path('atualizar-produto/<str:pk>', views.updateProduct, name='atualizar-produto'),
-    path('excluir-produto/<str:pk>', views.excludeProduct, name='excluir-produto')
+    path('excluir-produto/<str:pk>', views.excludeProduct, name='excluir-produto'),
+    path('mostrar-clientes/', views.showCustomers, name='mostrar-clientes'),
+    path('mostrar-cliente/<str:pk>', views.showCustomer, name='mostrar-cliente'),
+    path('criar-cliente/', views.createCustomer, name='criar-cliente'),
+    path('atualizar-cliente/<str:pk>', views.updateCustomer, name='atualizar-cliente'),
+    path('excluir-cliente/<str:pk>', views.excludeCustomer, name='excluir-cliente')
 ]
