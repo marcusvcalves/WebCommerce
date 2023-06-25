65408e4dae8ba82fe970324809e14161784f522e
Marcus Vinícius <76022387+marcusvcalves@users.noreply.github.com>
2023-06-25 12:57:05 -0300

    Update produtoCard.js

--- frontend/src/Components/produtoCard/produtoCard.js
+++ frontend/src/Components/produtoCard/produtoCard.js
@@ -4,13 +4,11 @@ import Button from '@mui/material/Button';
 export const ProdutoCard = ({id, name, image, description, price}) => {
     return (
             <div className='produtoCard' key={id}>
-                <div className='produto'>
-                    <img src={image} alt="produto" width='100%' height='200vh'></img>
-                    <h2 className='produto-text'>{name}</h2>
-                    <p className='produto-text produto-price'>R$ {price}</p>
-                    <div className='produto-footer'>
-                        <Button className='comprar-button' variant="contained">Comprar</Button>
-                    </div>
+                <img src={image} alt="produto" width='100%' height='200vh'></img>
+                <h2 className='produto-text'>{name}</h2>
+                <p className='produto-text produto-price'>R$ {price}</p>
+                <div className='produto-footer'>
+                    <Button className='comprar-button' variant="contained">Comprar</Button>
                 </div>
             </div>
     )
