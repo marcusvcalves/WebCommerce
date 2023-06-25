ab841ef305b20b7d48619fc9f68ba70f89a90463
Marcus Vinícius <76022387+marcusvcalves@users.noreply.github.com>
2023-06-25 12:34:20 -0300

    Update produtoCard.js

--- frontend/src/Components/produtoCard/produtoCard.js
+++ frontend/src/Components/produtoCard/produtoCard.js
@@ -7,8 +7,8 @@ export const ProdutoCard = ({id, name, image, description, price}) => {
                 <div className='produto'>
                     <img src={image} alt="produto" width='100%' height='200vh'></img>
                     <h2 className='produto-text'>{name}</h2>
-                    <p className='produto-text'>R$ {price}</p>
-                    <div className='produtoCard-footer'>
+                    <p className='produto-text produto-price'>R$ {price}</p>
+                    <div className='produto-footer'>
                         <Button className='comprar-button' variant="contained">Comprar</Button>
                     </div>
                 </div>
